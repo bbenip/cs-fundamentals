f97ee036c27f4d8d52687adf67cda8498ef2bfed
Baljinder Benipal <bbenipal10@gmail.com>
2019-03-13 23:59:13 -0400

    Create a main file to test linked list

---
+++ data-structures/linked-list/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "linked-list.h"
+
+using namespace std;
+
+int main() {
+  LinkedList l1;
+  l1.insertAtFront(2);
+  l1.insertAtBack(3);
+  l1.insertAtFront(1);
+
+  cout << "l1 size: " << l1.getSize() << endl;
+  cout << l1 << endl;
+
+  LinkedList l2;
+  l2.insertAtBack(3);
+  l2.insertAtFront(2);
+  l2.insertAt(0, 1);
+  l2.insertAt(3, 5);
+  l2.insertAt(3, 4);
+  l2.addRange(6, 10);
+
+  cout << "l2 size: " << l2.getSize() << endl;
+  cout << l2 << endl;
+}
